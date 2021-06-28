efea14dfd48f3f98a17d61fa93ff9bcae220f715
reyhane javivi <1100.reyhanej@gmail.com>
2021-06-28 18:51:11 +0430

    created server folder and installed mongoose

--- playground/mongo-connect.js
+++ playground/mongo-connect.js
@@ -11,22 +11,22 @@ client.connect((err) => {
         //     if (err) throw new Error("Unable to insrt values in the collection", err)
         //     console.log(JSON.stringify(result.ops))
         // })
-    db.collection("ToDos").insertOne({
-            text: "to read book",
-            completed: true
-        }, (err, result) => {
-            if (err) throw new Error("Unable to insrt values in the collection", err)
-            console.log(JSON.stringify(result.ops[0]._id.getTimestamp()))
-        })
-        // db.collection("Users").insertMany([{
-        //     name: "Morteza",
-        //     age: 24
-        // }, {
-        //     name: "sara",
-        //     age: 19
-        // }], (err, result) => {
-        //     if (err) throw new Error("Unable to insrt values in the collection")
-        //     console.log(JSON.stringify(result.ops, result.ops[1]._id))
-        // })
+        // db.collection("ToDos").insertOne({
+        //         text: "to read book",
+        //         completed: true
+        //     }, (err, result) => {
+        //         if (err) throw new Error("Unable to insrt values in the collection", err)
+        //         console.log(JSON.stringify(result.ops[0]._id.getTimestamp()))
+        //     })
+    db.collection("Users").insertMany([{
+        name: "Morteza",
+        age: 24
+    }, {
+        name: "sara",
+        age: 19
+    }], (err, result) => {
+        if (err) throw new Error("Unable to insrt values in the collection")
+        console.log(JSON.stringify(result.ops, result.ops[1]._id))
+    })
     client.close()
 })
--- playground/mongo-delete.js
+++ playground/mongo-delete.js
@@ -4,6 +4,15 @@ client.connect((err) => {
     if (err) return "Unable to connet to MongoDb"
     console.log("Connected to MongoDb successfully")
     const db = client.db("ToDoApp")
+        // db.collection("Users").deleteMany({ name: "ali" }).then(docs => {
+        //     console.log(docs)
+        // })
+        // db.collection("ToDos").deleteOne({ completed: true }).then(docs => {
+        //     console.log(docs)
+        // })
+    db.collection("ToDos").findOneAndDelete({ completed: true }).then(docs => {
+        console.log(docs)
+    })
 
     client.close()
 })
