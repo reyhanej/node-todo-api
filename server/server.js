69981b27d7ef03504f005e9f3acb24d55dd342e1
reyhane javivi <1100.reyhanej@gmail.com>
2021-07-02 20:42:06 +0430

    used get method with  paramerts

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 mongoose.connect('mongodb://localhost:27017/ToDoApp', { useNewUrlParser: true, useUnifiedTopology: true });
 const ToDo = require("./db/models/todo")
+const { ObjectID } = require("mongodb")
 const user = require("./db/models/user")
 const express = require("express")
 let app = express()
@@ -21,8 +22,21 @@ app.post("/todos", async(req, res) => {
         res.status(404)
         res.send(err)
     }
-
 });
+
+app.get("/todos", (req, res) => {
+    ToDo.find().then(todos => {
+        res.send({ todos })
+    }, (e) => res.status(404).send(e))
+})
+app.get("/todos:id", (req, res) => {
+    const id = req.params.id
+    if (!ObjectID.isValid(id)) { return res.status(404).send() }
+    ToDo.findById(id).then(todo => {
+        if (!todo) return res.status(404).send()
+        res.send({ todo })
+    }).catch(e => res.status(400).send(e))
+})
 
 app.listen(3000, () => console.log("server on 3000 port is running"))
 
